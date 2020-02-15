c0271718c6d3003fea58ea6c1ff88f2c3381fb3c
yjeom <44234192+yjeom@users.noreply.github.com>
2020-02-15 21:14:53 +0900

    jpa

--- src/main/java/com/yjeom/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/yjeom/book/springboot/domain/posts/Posts.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +29,9 @@ public class Posts {
         this.content=content;
         this.author=author;
     }
+    public void update(String title,String content){
+        this.title=title;
+        this.content=content;
+    }
+
 }
