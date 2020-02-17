080f4fa2146d0225d11b8f0cefa7218510ea4efc
yjeom <44234192+yjeom@users.noreply.github.com>
2020-02-17 16:52:34 +0900

    게시판 CRUD

--- src/main/java/com/yjeom/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yjeom/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.yjeom.book.springboot.service.posts;
 
 import com.yjeom.book.springboot.domain.posts.Posts;
 import com.yjeom.book.springboot.domain.posts.PostsRepository;
+import com.yjeom.book.springboot.web.dto.PostsListResponseDto;
 import com.yjeom.book.springboot.web.dto.PostsResponseDto;
 import com.yjeom.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yjeom.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,16 @@ public class PostsService {
                 ()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id)
         );
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly=true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+    @Transactional
+    public void delete(Long id){
+        Posts posts=postsRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("해당게시글이없습니다.id="+id));
+        postsRepository.delete(posts);
     }
 }
