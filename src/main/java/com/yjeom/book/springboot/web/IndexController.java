2b843605d4ad88e210f9148208f981b00c3b0850
yjeom <44234192+yjeom@users.noreply.github.com>
2020-02-17 21:38:02 +0900

    소셜로그인google

---
+++ src/main/java/com/yjeom/book/springboot/web/IndexController.java
@@ -0,0 +1,42 @@
+package com.yjeom.book.springboot.web;
+
+import com.yjeom.book.springboot.config.auth.dto.SessionUser;
+import com.yjeom.book.springboot.service.posts.PostsService;
+import com.yjeom.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+    @GetMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user=(SessionUser)httpSession.getAttribute("user");
+        if(user !=null){
+            model.addAttribute("memberName",user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id,Model model){
+        PostsResponseDto dto=postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+
+}
