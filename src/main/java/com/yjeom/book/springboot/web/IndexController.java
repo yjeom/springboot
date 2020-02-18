e8661e33a2ecab21f358437099676181cfe68b38
yjeom <44234192+yjeom@users.noreply.github.com>
2020-02-18 18:12:10 +0900

    소셜로그인naver

--- src/main/java/com/yjeom/book/springboot/web/IndexController.java
+++ src/main/java/com/yjeom/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yjeom.book.springboot.web;
 
+import com.yjeom.book.springboot.config.auth.LoginUser;
 import com.yjeom.book.springboot.config.auth.dto.SessionUser;
 import com.yjeom.book.springboot.service.posts.PostsService;
 import com.yjeom.book.springboot.web.dto.PostsResponseDto;
@@ -18,10 +19,10 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user=(SessionUser)httpSession.getAttribute("user");
+
         if(user !=null){
             model.addAttribute("memberName",user.getName());
         }
