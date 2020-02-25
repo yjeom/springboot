c773a8d61298152e75b3e9c73c75ffa42ea646c2
yjeom <44234192+yjeom@users.noreply.github.com>
2020-02-25 15:22:00 +0900

    수정

--- src/main/java/com/yjeom/book/springboot/Application.java
+++ src/main/java/com/yjeom/book/springboot/Application.java
@@ -2,9 +2,11 @@ package com.yjeom.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
+@ComponentScan({"com.yjeom.book.springbook"})
 @SpringBootApplication
 public class Application {
     public static void main(String[]args){
